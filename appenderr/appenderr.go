f1dec8b6e240bb3e5c5138b98a09afaa428f7379
Joshua Smith <joshua.smith@rakuten.com>
2018-01-25 14:19:35 -0700

    Add AppendError

---
+++ appenderr/appenderr.go
@@ -0,0 +1,89 @@
+package appenderr
+
+import (
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+// AppendErr a helper error struct that counts and removes duplicates errors.
+// errors are considered identical if their error signature is the same.
+type AppendErr struct {
+	mu   sync.RWMutex
+	errs map[string]errData
+}
+
+type errData struct {
+	err   error
+	count int
+	t     time.Time
+}
+
+// New AppendErr
+func New() *AppendErr {
+	e := &AppendErr{
+		errs: make(map[string]errData, 0),
+	}
+	return e
+}
+
+// Add an error to stack, appendErr will ignore nil errors
+func (e *AppendErr) Add(err error) {
+	if err == nil {
+		return
+	}
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	if appendErr, ok := err.(*AppendErr); ok {
+		for key, aErr := range appendErr.errs {
+			if d, found := e.errs[key]; found {
+				d.count += aErr.count
+				e.errs[key] = d
+			} else {
+				e.errs[key] = aErr
+			}
+		}
+		return
+	}
+	key := err.Error()
+	if d, found := e.errs[key]; found {
+		d.count++
+		e.errs[key] = d
+	} else {
+		e.errs[key] = errData{
+			err:   err,
+			count: 1,
+			t:     time.Now(),
+		}
+	}
+	return
+}
+
+func (e *AppendErr) Error() (s string) {
+
+	errs := make([]errData, 0, len(e.errs))
+	e.mu.RLock()
+	for _, err := range e.errs {
+		errs = append(errs, err)
+	}
+	e.mu.RUnlock()
+
+	// sort if needed
+
+	for _, err := range errs {
+		if err.count > 1 {
+			s += "[" + strconv.Itoa(err.count) + "] "
+		}
+		s += err.err.Error() + "\n"
+	}
+	return strings.TrimRight(s, "\n")
+}
+
+// ErrOrNil returns itself if errors are found or nil
+func (e *AppendErr) ErrOrNil() error {
+	if len(e.errs) == 0 {
+		return nil
+	}
+	return e
+}
---
+++ appenderr/appenderr_test.go
@@ -0,0 +1,81 @@
+package appenderr
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAdd(t *testing.T) {
+	cases := []struct {
+		msg      string
+		errs     []error
+		expected string
+		isNil    bool
+	}{
+		{
+			msg:   "No Errors",
+			isNil: true,
+		},
+		{
+			msg:      "3 identical errors",
+			errs:     makeErrs("err", "err", "err"),
+			expected: "[3] err",
+		},
+		{
+			msg:      "3 unique errors",
+			errs:     makeErrs("err3", "err2", "err1"),
+			expected: "err3\nerr2\nerr1",
+		},
+		{
+			msg: "add append errors",
+			errs: append(makeErrs("err1", "err2"), &AppendErr{
+				errs: map[string]errData{
+					"err1": errData{
+						err:   fmt.Errorf("err1"),
+						count: 1,
+						t:     time.Now(),
+					},
+					"err3": errData{
+						err:   fmt.Errorf("err3"),
+						count: 2,
+						t:     time.Now(),
+					},
+				},
+			}),
+			expected: "[2] err1\nerr2\n[2] err3",
+		},
+		{
+			msg:      "errors with one nil",
+			errs:     []error{(error)(nil), fmt.Errorf("err1")},
+			expected: "err1",
+		},
+		{
+			msg:   "nil error",
+			errs:  []error{(error)(nil)},
+			isNil: true,
+		},
+	}
+	for _, test := range cases {
+		aErr := New()
+		for _, err := range test.errs {
+			aErr.Add(err)
+		}
+		if test.isNil != (aErr.ErrOrNil() == nil) {
+			t.Errorf("FAIL: %s", test.msg)
+		} else if !cmp.Equal(aErr.Error(), test.expected) {
+			t.Errorf("FAIL: %s %s", test.msg, cmp.Diff((aErr.Error()), test.expected))
+		} else {
+			t.Logf("PASS: %s", test.msg)
+		}
+	}
+}
+
+func makeErrs(data ...string) (errs []error) {
+	for _, v := range data {
+		errs = append(errs, fmt.Errorf(v))
+	}
+	return errs
+}
